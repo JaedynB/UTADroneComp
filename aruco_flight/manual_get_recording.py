3d6622045ed08460dd3099f4eb8cd718b0f0c327
TopDroneMavericks <topdronemavs@gmail.com>
2023-02-21 18:00:05 +0000

    shortened cable test footage

--- aruco_flight/manual_get_recording.py
+++ aruco_flight/manual_get_recording.py
@@ -11,31 +11,31 @@ import numpy     as np
 
 connection_string = '/dev/ttyUSB0'
 print('Connecting to vehicle on: ', connection_string)
-#vehicle = connect(connection_string,baud=57600,wait_ready=True)
+vehicle = connect(connection_string,baud=57600,wait_ready=True)
 print('Connected')
 
 print("Starting mission")
-#if vehicle.mode != 'STABILIZE':
-#    vehicle.wait_for_mode('STABILIZE')
-#    print('Mode: ', vehicle.mode)
+if vehicle.mode != 'STABILIZE':
+    vehicle.wait_for_mode('STABILIZE')
+    print('Mode: ', vehicle.mode)
     
 print('Arming...')
-#vehicle.arm()
-#if vehicle.armed == True:
-#    print('Armed')
-#else:
-#    print('Could not arm...')
+vehicle.arm()
+if vehicle.armed == True:
+    print('Armed')
+else:
+    print('Could not arm...')
 ###########################################
 picam2 = Picamera2()
-picam2.configure(picam2.create_video_configuration())
-"""
-picam2.resolution = (640, 480)
-picam2.start_and_record_video("test.mp4", duration = 20)
-"""
+#picam2.configure(picam2.create_video_configuration())
 
-encoder = H264Encoder()
-output = "test.h264"
-picam2.start_recording(encoder, 'test.h264', Quality.HIGH)
+picam2.resolution = (640, 480)
+picam2.start_and_record_video("test.mp4", duration = 30)
+
+
+#encoder = H264Encoder()
+#output = "test.h264"
+#picam2.start_recording(encoder, 'test.h264', Quality.HIGH)
 
 time.sleep(30)
 picam2.stop_recording()
