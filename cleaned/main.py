2b3eb57c66dbd8c05009251941af72c5a25aadf1
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2023-04-11 17:27:02 -0500

    Added bots to program from outside file

--- cleaned/main.py
+++ cleaned/main.py
@@ -8,6 +8,7 @@ from   cv2                import aruco
 from   missions           import arm_and_takeoff
 from   initialize         import initialize
 from   aruco_detector     import ArUcoDetector
+from   irc_bot_utils      import IRCBot, UAVBot, UGVHitListener, run_bot
 import numpy              as np
 import time
 import cv2
@@ -23,6 +24,22 @@ import threading
 # Initializing PiCamera2 object with main preview configuration set to XRGB8888 format and 640x480 resolution
 picam2.configure(picam2.create_preview_configuration(main = {"format": 'XRGB8888', "size": (640, 480)}))
 picam2.start()
+
+print("Creating the IRC bots...")
+
+# Create and start the UAVBot in a separate thread
+uav_bot    = UAVBot()
+uav_thread = threading.Thread(target = run_bot, args = (uav_bot,))
+uav_thread.start()
+
+# Create and start the UGVHitListener in a separate thread
+listener        = UGVHitListener()
+listener_thread = threading.Thread(target = run_bot, args = (listener,))
+listener_thread.start()
+
+# Wait for the UAVBot and UGVHitListener to connect to the server
+while not uav_bot.connected and not listener.connected:
+    time.sleep(1)
 
 vehicle, aruco_detector = initialize()
 
